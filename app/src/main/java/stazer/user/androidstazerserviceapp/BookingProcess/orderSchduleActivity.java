b8067d8b8b72f389a93c6780c3b813746eef7449
Siddhant Jain <53446186+Siddhantjan@users.noreply.github.com>
2020-12-15 22:37:02 +0530

    some updates in schedule activity and set range to select date and add some restriction

--- app/src/main/java/stazer/user/androidstazerserviceapp/BookingProcess/orderSchduleActivity.java
+++ app/src/main/java/stazer/user/androidstazerserviceapp/BookingProcess/orderSchduleActivity.java
@@ -1,23 +1,23 @@
 package stazer.user.androidstazerserviceapp.BookingProcess;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
-
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,26 +25,27 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Objects;
 
 import stazer.user.androidstazerserviceapp.BookingInfo.BookingActivity;
-import stazer.user.androidstazerserviceapp.BookingProcess.dateTimePicker.DateFragment;
-import stazer.user.androidstazerserviceapp.BookingProcess.dateTimePicker.TimeFragment;
 import stazer.user.androidstazerserviceapp.Common.Common;
-import stazer.user.androidstazerserviceapp.MainActivity;
 import stazer.user.androidstazerserviceapp.R;
 
 public class orderSchduleActivity extends AppCompatActivity {
 
     private TextView mSelectTime, mSelectDate;
-    private EditText mDisplayTime, mDisplayDate;
+    private TextView mDisplayTime, mDisplayDate;
     FirebaseDatabase database;
     DatabaseReference userInfoRef,adminInfoRef;
     private TextView mServiceType;
     private Button btn_bookingSchedule;
     private TextView mName, mMobileNumber,  mAddress;
     private String UserAddress;
+
+    private int cYear,cMonth,cDay,sYear,sMonth,sDay;
+    private int cHour,cMinute,sHour,sMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,18 +54,95 @@ public class orderSchduleActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_order_schdule);
 
+        //initialize calender
+        Calendar calendar = Calendar.getInstance();
+
+        //get Current Year
+        cYear = calendar.get(Calendar.YEAR);
+        //get Current Month
+        cMonth = calendar.get(Calendar.MONTH);
+        // get Current Day
+        cDay = calendar.get(Calendar.DAY_OF_MONTH);
+        // Get Current Hour
+        cHour = calendar.get(Calendar.HOUR_OF_DAY);
+        // Get Current Minutes
+        cMinute = calendar.get(Calendar.MINUTE);
+
         //these are invisible hooks
         mName = findViewById(R.id.customer_name_sch);
         mMobileNumber = findViewById(R.id.customer_mobile_sch);
         mAddress = findViewById(R.id.address_type_sch);
 
-        //Hooks
+        //Hooks for Date and Time
         mSelectDate = (TextView) findViewById(R.id.btn_booking_date);
         mSelectTime = (TextView) findViewById(R.id.btn_booking_time);
-        mDisplayDate = (EditText) findViewById(R.id.btn_booking_date_display);
-        mDisplayTime = (EditText) findViewById(R.id.btn_booking_time_display);
+        mDisplayDate = (TextView) findViewById(R.id.btn_booking_date_display);
+        mDisplayTime = (TextView) findViewById(R.id.btn_booking_time_display);
+
+        mSelectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Initialize date Picker Dialog
+                DatePickerDialog datePickerDialog = new DatePickerDialog(orderSchduleActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        sYear=year;
+                        sMonth = month;
+                        sDay = dayOfMonth;
+
+                        String sDate = sDay+"-"+sMonth+"-"+sYear;
+                        mDisplayDate.setText(sDate);
+
+                    }
+                },cYear,cMonth,cDay);
+                // Displayed previous selected Date
+                datePickerDialog.updateDate(sYear,sMonth,sDay);
+                //Disabled past Date
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                // Disabled future Date
+                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis() + 604800000L); // 7 * 24 * 60 * 60 * 1000
+                //show Date Picker Dialog
+                datePickerDialog.show();
+            }
+        });
+
+        mSelectTime.setOnClickListener(v -> {
+            //Initialize Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(
+                    orderSchduleActivity.this, (view, hourOfDay, minute) -> {
+                sHour = hourOfDay;
+                sMinute = minute;
+
+                //Initialize Calender
+                Calendar calendar1= Calendar.getInstance();
+
+                String sDate = mDisplayDate.getText().toString().trim();
+                //Split Date
+                String [] strings = sDate.split("-");
+                //Get day on Calender
+                sDay = Integer.parseInt(strings[0]);
+                //set day on Calender
+                calendar1.set(Calendar.DAY_OF_MONTH,sDay);
+                //set Hour on Calender
+                calendar1.set(Calendar.HOUR_OF_DAY,sHour);
+                //set Minute on Calender
+                calendar1.set(Calendar.MINUTE,sMinute);
+                if (calendar1.getTimeInMillis() >= Calendar.getInstance().getTimeInMillis()){
+                    mDisplayTime.setText(DateFormat.format("hh:mm aa",calendar1));
+                }
+                else {
+                    Toast.makeText(this, "You Selected Past Time Please Select Correct Time", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }, cHour,cMinute,false);
+            //show Dialog
+            timePickerDialog.show();
+        });
+
         mServiceType = findViewById(R.id.service_type_sch);
         btn_bookingSchedule = findViewById(R.id.btn_booking_schdule);
+
         mServiceType.setText(getIntent().getStringExtra("serviceType"));
         database = FirebaseDatabase.getInstance();
         userInfoRef = database.getReference(Common.USER_INFO_REFERENCE);
@@ -151,30 +229,4 @@ public class orderSchduleActivity extends AppCompatActivity {
         finish();
     }
 
-    public void CalendarAppears(View view) {
-        DialogFragment dialogFragment = new DateFragment();
-        dialogFragment.show(getSupportFragmentManager(), "Select Date");
-    }
-
-    public void takeDate(int year, int month, int date) {
-        String Years = Integer.toString(year);
-        String Month = Integer.toString(month);
-        String Date = Integer.toString(date);
-        mDisplayDate.setVisibility(View.VISIBLE);
-        mDisplayDate.setText(Date + "/" + Month + "/" + Years);
-
-    }
-
-    public void TimeAppears(View view) {
-        DialogFragment fragment = new TimeFragment();
-        fragment.show(getSupportFragmentManager(), "Select Time");
-    }
-
-    public void takeTime(int hourOfDay, int minute) {
-        String hour = Integer.toString(hourOfDay);
-        String min = Integer.toString(minute);
-
-        mDisplayTime.setVisibility(View.VISIBLE);
-        mDisplayTime.setText(hour + ":" + min );
-    }
 }
