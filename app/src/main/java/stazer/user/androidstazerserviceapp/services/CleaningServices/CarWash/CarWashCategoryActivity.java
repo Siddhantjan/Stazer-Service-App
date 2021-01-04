ae76d01e0742247cdbd5391f8207a78c98dc9f8c
Siddhant Jain <53446186+Siddhantjan@users.noreply.github.com>
2021-01-04 10:53:02 +0530

    add price from Fetched Database

--- app/src/main/java/stazer/user/androidstazerserviceapp/services/CleaningServices/CarWash/CarWashCategoryActivity.java
+++ app/src/main/java/stazer/user/androidstazerserviceapp/services/CleaningServices/CarWash/CarWashCategoryActivity.java
@@ -1,5 +1,6 @@
 package stazer.user.androidstazerserviceapp.services.CleaningServices.CarWash;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,15 @@ import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 import stazer.user.androidstazerserviceapp.BookingProcess.OrderBookingActivity;
 import stazer.user.androidstazerserviceapp.BookingProcess.orderSchduleActivity;
@@ -17,13 +27,36 @@ import stazer.user.androidstazerserviceapp.R;
 public class CarWashCategoryActivity extends AppCompatActivity {
    private String serviceName;
     NetworkChangeListener networkChangeListener = new NetworkChangeListener();
-
+    TextView mEcoWash,mStdWash,mPremiumWash;
     @Override
     protected void onStart() {
         IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
         registerReceiver(networkChangeListener,filter);
-        super.onStart();
         serviceName = getIntent().getStringExtra("serviceTypeCar");
+        String nameOfCar = getIntent().getStringExtra("ServiceName");
+        FirebaseDatabase.getInstance().getReference().child("RateCards").child("CarWashingRates")
+                .child(nameOfCar)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()){
+                            String  EcoWash  = Objects.requireNonNull(snapshot.child("EcoWash").getValue()).toString();
+                            String  StdWash = Objects.requireNonNull(snapshot.child("StdWash").getValue()).toString();
+                            String  PremiumWash = Objects.requireNonNull(snapshot.child("PremiumWash").getValue()).toString();
+                            mEcoWash.setText("Price :"+"₹"+EcoWash);
+                            mStdWash.setText("Price :"+"₹"+StdWash);
+                            mPremiumWash.setText("Price :"+"₹"+PremiumWash);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(CarWashCategoryActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        super.onStart();
+
     }
     @Override
     protected void onStop() {
@@ -38,6 +71,9 @@ public class CarWashCategoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_car_wash_category);
 
         // assign service name
+        mEcoWash = findViewById(R.id.priceEco);
+        mStdWash= findViewById(R.id.priceStandard);
+        mPremiumWash = findViewById(R.id.pricePremium);
 
 
         findViewById(R.id.ecoWash).setOnClickListener(v -> {
--- app/src/main/java/stazer/user/androidstazerserviceapp/services/CleaningServices/CarWash/CarWashingActivity.java
+++ app/src/main/java/stazer/user/androidstazerserviceapp/services/CleaningServices/CarWash/CarWashingActivity.java
@@ -19,28 +19,35 @@ public class CarWashingActivity extends AppCompatActivity {
         findViewById(R.id.hatchbackCar).setOnClickListener(v -> {
             Intent carWashServiceIntent = new Intent(getApplicationContext(), CarWashCategoryActivity.class);
             carWashServiceIntent.putExtra("serviceTypeCar","HatchBack Car Wash Service");
+            carWashServiceIntent.putExtra("ServiceName","HatchBack");
             startActivity(carWashServiceIntent);
         });
         findViewById(R.id.sedanCar).setOnClickListener(v -> {
             Intent carWashServiceIntent = new Intent(getApplicationContext(), CarWashCategoryActivity.class);
             carWashServiceIntent.putExtra("serviceTypeCar","Sedan Car Wash Service");
+            carWashServiceIntent.putExtra("ServiceName","Sedan");
             startActivity(carWashServiceIntent);
         });
 
         findViewById(R.id.seaterXUV_5).setOnClickListener(v -> {
             Intent carWashServiceIntent = new Intent(getApplicationContext(), CarWashCategoryActivity.class);
-            carWashServiceIntent.putExtra("serviceTypeCar","5 Seater XUV Car Wash Service");
+            carWashServiceIntent.putExtra("serviceTypeCar","5 Seater SUV Car Wash Service");
+            carWashServiceIntent.putExtra("ServiceName","5_seater_SUV");
             startActivity(carWashServiceIntent);
         });
         findViewById(R.id.seaterXUV_7).setOnClickListener(v -> {
             Intent carWashServiceIntent = new Intent(getApplicationContext(), CarWashCategoryActivity.class);
-            carWashServiceIntent.putExtra("serviceTypeCar","7 Seater XUV Car Wash Service");
+            carWashServiceIntent.putExtra("serviceTypeCar","7 Seater SUV Car Wash Service");
+            carWashServiceIntent.putExtra("ServiceName","7_seater_SUV");
+
             startActivity(carWashServiceIntent);
         });
 
         findViewById(R.id.luxury).setOnClickListener(v -> {
             Intent carWashServiceIntent = new Intent(getApplicationContext(), CarWashCategoryActivity.class);
             carWashServiceIntent.putExtra("serviceTypeCar","Luxury Car Wash Service");
+            carWashServiceIntent.putExtra("ServiceName","Luxury");
+
             startActivity(carWashServiceIntent);
         });
     }
